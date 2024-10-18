cb1df310b8ec0c4968c82867f8ed1eae9047b2b8
Karolis Bandziulis <g00417529@atu.ie>
2024-10-18 14:01:35 +0000

    Lab 4 complete

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,7 +1,32 @@
+import { useState } from "react";
+
 // Read content to be imported
 const Create = () => {
-  return <h3>Hello from the Read Create component</h3>;
-};
+  // state var
+  const [title, setTitle] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault(); // stops from being fired more than once
+    console.log(title);
+  }
+
+  return (
+    <div>
+      <h3>Hello from the Read Create component</h3>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Add Movie Title: </label>
+          <input type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }}></input>
+        </div>
+        <div>
+          <input type="submit" value="Add Movie"></input>
+        </div>
+      </form>
+    </div>
+  );
+}
 
 // exporting module to be used in app.js
 export default Create;
