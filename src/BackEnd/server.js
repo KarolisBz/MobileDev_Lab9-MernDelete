f30581ae79541c51ce075c353d90260341ceedf2
Karolis Bandziulis <g00417529@atu.ie>
2024-11-08 15:03:13 +0000

    4. Connect the React App to Read Data from the Node/Express Server

--- src/BackEnd/server.js
+++ src/BackEnd/server.js
@@ -2,37 +2,54 @@
 const express = require('express');
 const app = express();
 const port = 4000;
-// all the movie data
-const movieData = {
-    "movies": [
-      {
-        "Title": "Avengers: Infinity War (online)",
-        "Year": "2018",
-        "imdbID": "tt4154756",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-      },
-      {
-        "Title": "Captain America: Civil War (online)",
-        "Year": "2016",
-        "imdbID": "tt3498820",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-      },
-      {
-        "Title": "World War Z (online)",
-        "Year": "2013",
-        "imdbID": "tt0816711",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-      }
-    ]
-  }
+
+// when browser connects to server, it's okay to accept requests outside domain
+const cors = require('cors');
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // if we get a request, 'Welcome to Data Respresentation & Querying'
 app.get('/api/movies', (req, res) => {
+    // all the movie data
+    const movieData = {
+        "movies": [
+        {
+            "Title": "Avengers: Infinity War (online)",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War (online)",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+        },
+        {
+            "Title": "World War Z (online)",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+        }
+        ]
+    }
+
     res.send(movieData);
+    //res.status(200).json({myMovies})
 });
+
+app.post('/api/movies',(req,res) => {
+    res.send("Movies Added!");
+})
 
 // severs listens for a http request coming in
 app.listen(port, () => {
